8fd0d800906c048c27f395f3f59f51a9435135da
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2021-12-29 22:38:21 -0300

    Fix credo consistency warning

--- lib/cuex/converter.ex
+++ lib/cuex/converter.ex
@@ -86,12 +86,12 @@ defmodule Cuex.Converter do
   end
 
   def convert_currency(
-        params = %{
+        %{
           "from_currency" => from_currency,
           "to_currency" => to_currency,
           "value" => value,
           "user_id" => _user_id
-        }
+        } = params
       ) do
     with {:ok, response} <- fetch_exchange_rate(),
          {:ok, exchange_rates} <-
