be04779ea414afef73f489733ccd1d456b71ba5d
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2021-12-31 12:27:48 -0300

    update exchangerate prod api_key

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,9 @@ import Config
 config :cuex, :exchangerate,
   api: Cuex.Api.Real.ExchangeRate,
   url: "http://api.exchangeratesapi.io/v1/latest",
-  api_key: System.get_env("EXCHANGERATE_API_KEY")
+  # Heroku does not offer a build_time secrets manager, could fix this using Github action + AWS
+  # Hardcoding here
+  api_key: "bd0de952f4ceedd0494499718f0c0a04"
 
 # Do not print debug messages in production
 config :logger, level: :info
