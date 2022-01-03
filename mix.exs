1a2e9efc53f7d3312386905cdf16cb6aa131e35c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-01-03 11:57:58 -0300

    Add scrivener paginator

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Cuex.MixProject do
       {:phoenix, "~> 1.6.5"},
       {:plug_cowboy, "~> 2.5"},
       {:poison, "~> 2.0"},
+      {:scrivener_ecto, "~> 2.0"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
 
