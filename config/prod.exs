ef796b0c9c34ee2d9218a435b675a7580af74d05
Gabriel Fabian <61850709+gabriel-fabian@users.noreply.github.com>
2022-04-28 13:03:53 -0300

    chore: change API KEY

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,9 @@ config :cuex, :exchangerate,
   url: "http://api.exchangeratesapi.io/v1/latest",
   # Heroku does not offer a build_time secrets manager, could fix this using Github action + AWS
   # Hardcoding here
-  api_key: "bd0de952f4ceedd0494499718f0c0a04"
+  # TODO Remove the keys
+  # api_key: "bd0de952f4ceedd0494499718f0c0a04"
+  api_key: "f24f191936990dd0d2ce176d62568af2"
 
 # Do not print debug messages in production
 config :logger, level: :info
